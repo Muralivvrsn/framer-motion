c1441372e2c30ed63b876a68c15c770e0db0f9c2
Muralivvrsn <64121609+Muralivvrsn@users.noreply.github.com>
2023-11-17 19:25:26 +0000

    dfghjkl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
 import './App.css';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
+  const [show, setShow] = useState(true);
+
   return (
-    <div className="App">
-      
+    <div className='help'>
+      <div className='one'>
+        <button onClick={()=>setShow(false)} className={`btn ${show?"show":"not-show"}`}>Match Preference</button>
+        <button onClick={()=>setShow(true)} className={`btn ${!show?"not-show":"show"}`}>save</button>
+      </div>
+      <div className={`two ${!show?"level-show":""}`}>
+      <button className={`btn`}>Entry Level</button>
+      <button className={`btn`}>Employer Type</button>
+      <button className={`btn`}>Location</button>
+      </div>
+      <div className={`three ${!show?'submit-show':'submit-not-show'}`}>
+      <button className={`btn`}>Upload</button>
+      <button className={`btn`}>Submit</button>
+      </div>
     </div>
   );
 }
